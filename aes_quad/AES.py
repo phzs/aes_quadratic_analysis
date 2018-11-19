251f013689f6a58bcf56b2eb23ac260d02c7a921
phzs <zsph1012@hs-karlsruhe.de>
2018-11-19 14:28:18 +0100

    Cleans up

--- aes_quad/AES.py
+++ aes_quad/AES.py
@@ -8,15 +8,17 @@ key_length = 4
 key_amount = 11
 
 var('x a')
-#k = GF(2**8, x, modulus)
-P = PolynomialRing(GF(2), x) # to enforce modulo comp.
-modulus = x**8 + x**4 + x**3 + x + 1
-F = GF(2**8, a, modulus)
+key_variables = list(var(" ".join("k%d" % i for i in xrange(8*16))))
+F = GF(2**8, 'a') 
+F.modulus = a**8 + a**4 + a**3 + a + 1
+z = F.gen()
 
 # key bit variables (8*16 for AES128)
-key_variables = var(" ".join("k%d" % i for i in xrange(8*16)))
-#var('k0 k1 k2 k3 k4 k5 k6 k7 k8 k9 k10 k11 k12 k13 k14 k15')
+key_char = 'k'
+key_variable_names = [key_char + str(i) + str(j)
+                       for i in range(block_size/8) for j in range(8)]
 
+#uF = PolynomialRing(F, len(key_variable_names), key_variable_names)
 # list of key variables: key_length 32bit-vectors
 """
 K = [
@@ -32,23 +34,13 @@ for i in xrange(16):
     polynom = F(0)
     for j in xrange(8):
         key_index = (8*i)+j
-        polynom += F(key_variables[key_index] * (F.gen()**j))
+#        polynom += F(key_variables[key_index] * (F.gen()**j))
     key_polynomials.append(polynom)
 
 # key words
 K = []
-for i in xrange(N):
+for i in xrange(128/4):
     K.append(vector(F, key_polynomials[i:i+4]))
-
-# kann ersetzt werden mit k._cache.fetch_int(...)
-def polynom(numerical_repr):
-    result = k(0) # null polynom
-    print "init", result
-    for i in xrange(8):
-        coeff = int((numerical_repr & (1 << i)) != 0)
-        subpoly =  k(coeff * (k.gen()**i))
-        result += subpoly
-    return result
 
 # rotate a word [a b c d] to [d b c a]
 def RotWord(word):
@@ -58,7 +50,7 @@ def SubWord(word):
     return [S(word[0]), S(word[1]), S(word[2]), S(word[3])]
 
 # affine transformation
-SBox_inverseT = matrix(F, [
+SBox_inverseT = matrix(GF(2), [
         [0, 0, 1, 0, 0, 1, 0, 1],
         [1, 0, 0, 1, 0, 0, 1, 0],
         [0, 1, 0, 0, 1, 0, 0, 1],
@@ -68,9 +60,7 @@ SBox_inverseT = matrix(F, [
         [1, 0, 0, 1, 0, 1, 0, 0],
         [0, 1, 0, 0, 1, 0, 1, 0]
     ])
-print SBox_inverseT
 
 def S(polynom):
-
-    #TODO calculate multiplicative inverse
-    return polynom #TODO
+    #TODO return SBox_M * polynom + SBox_t
+    return polynom
--- aes_quad/main.py
+++ aes_quad/main.py
@@ -4,16 +4,21 @@ from sage.rings.finite_rings.finite_field_givaro import *
 from key_schedule import *
 import AES
 
-#k.<a> = GF(2^8)
-print "AES", AES.block_size
-print "modulus: ", AES.k.modulus()
-print "generator:", AES.k.gen()
+print "-"*13
+print "| AES ", AES.block_size, " |"
+print "-"*13
+print "modulus: ", AES.F.modulus()
+print "generator:", AES.F.gen()
 
+rc = AESKeySchedule.generate_rc()
 rc_test = [0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1B, 0x36]
-print "rc_test\t", rc_test
-print "rc\t", AESKeySchedule.generate_rc().values()
+assert rc_test == rc.values()
+print "rc\t", rc.values()
 
 schedule = AESKeySchedule()
-print schedule.W
 
-print AES.polynom(0x37)
+
+print
+print "KEY EXPANSION: W"
+for subkey_index in schedule.W:
+    print subkey_index, schedule.W[subkey_index]

dc2bff16894c1aa5cd4950393e7d19dd15383648
phzs <zsph1012@hs-karlsruhe.de>
2019-01-08 18:39:22 +0100

    Implements encrypt, decrypt and missing functions

--- aes_quad/AES.py
+++ aes_quad/AES.py
@@ -1,6 +1,6 @@
 from sage.all import *
+from itertools import izip_longest
 from key_schedule import AESKeySchedule
-
 
 """
 
@@ -34,6 +34,20 @@ SBox_inverse_M = matrix(GF(2), [
 
 SBox_inverse_t = vector(GF(2), [1, 0, 1, 0, 0, 0, 0, 0])
 
+shift_rows_M = [
+    0, 5, 10, 15,
+    4, 9, 14, 3,
+    8, 13, 12, 7,
+    12, 1, 6, 11
+]
+
+shift_rows_inverse_M = [
+    0, 13, 10, 7,
+    4, 1, 14, 11,
+    8, 5, 2, 6,
+    7, 11, 15, 3
+]
+
 def reverse(input_vector):
     tmp = input_vector.list()
     tmp.reverse()
@@ -50,6 +64,20 @@ def polynomial_from_vector(parent, input_vector):
 pgen = polygen(Integers(2))
 modulus = pgen()**8 + pgen()**4 + pgen()**3 + pgen() + 1
 gf = FiniteField(2**8, 'a', modulus=modulus)
+
+mix_columns_M = matrix(gf, [
+    [gf._cache.fetch_int(0x02), gf._cache.fetch_int(0x03), gf._cache.fetch_int(0x01), gf._cache.fetch_int(01)],
+    [gf._cache.fetch_int(0x01), gf._cache.fetch_int(0x02), gf._cache.fetch_int(0x03), gf._cache.fetch_int(01)],
+    [gf._cache.fetch_int(0x01), gf._cache.fetch_int(0x01), gf._cache.fetch_int(0x02), gf._cache.fetch_int(03)],
+    [gf._cache.fetch_int(0x03), gf._cache.fetch_int(0x01), gf._cache.fetch_int(0x01), gf._cache.fetch_int(02)],
+])
+
+mix_columns_inverse_M = matrix(gf, [
+    [gf._cache.fetch_int(0x0E), gf._cache.fetch_int(0x0B), gf._cache.fetch_int(0x0D), gf._cache.fetch_int(0x09)],
+    [gf._cache.fetch_int(0x09), gf._cache.fetch_int(0x0E), gf._cache.fetch_int(0x0B), gf._cache.fetch_int(0x0D)],
+    [gf._cache.fetch_int(0x0D), gf._cache.fetch_int(0x09), gf._cache.fetch_int(0x0E), gf._cache.fetch_int(0x0B)],
+    [gf._cache.fetch_int(0x0B), gf._cache.fetch_int(0x0D), gf._cache.fetch_int(0x09), gf._cache.fetch_int(0x0E)],
+])
 
 class AES(SageObject):
 
@@ -73,14 +101,38 @@ class AES(SageObject):
             self.key = None
 
     def encrypt(self, plaintext):
-        state = self.AddRoundKey([gf(0)] * 16, self.key)
-        for round in xrange(self.rounds):
-            #TODO perform AES round
-            print round
-        return state
+        result = []
+        for block in self.get_blocks(plaintext, " "):
+            converted_block = [gf._cache.fetch_int(ord(entry)) for entry in block]
+            state = self.AddRoundKey(converted_block, self.key)
+            for round in xrange(self.rounds):
+                state = self.SubBytes(state)
+                state = self.ShiftRows(state)
+                state = self.MixColumns(state)
+            result += state
+        return result
+
+
+    def get_blocks(self, sequence, fillvalue=None):
+        """
+        Returns iterable of blocks of size :self.block_size:, fills up with :fillvalue:
+        values = list(sequence)
+        size = self.block_size / 8
+        while (len(values) % size) is not 0:
+            values.append(fillvalue)
+
+        return (values[pos:pos + size] for pos in xrange(0, len(values), size))
 
     def decrypt(self, ciphertext):
-        pass
+        result = []
+        for block in self.get_blocks(ciphertext, " "):
+            state = self.AddRoundKeyInv(block, self.key)
+            for round in xrange(self.rounds):
+                state = self.SubBytesInv(state)
+                state = self.ShiftRowsInv(state)
+                state = self.MixColumnsInv(state)
+            result += state
+        return result
 
     def get_equations(self, known_plaintext, known_ciphertext, **kwargs):
         # key bit variables (8*16 for AES128)
@@ -111,19 +163,66 @@ class AES(SageObject):
     def AddRoundKey(self, state, key):
         return [(state[i] + key[i]) for i in xrange(self.block_size / 8)]
 
+    def AddRoundKeyInv(self, state, key):
+        inv_key = key
+        inv_key.reverse() # TODO better save this as a member to avoid recalculating each time
+        return self.AddRoundKey(state, inv_key)
+
+    def ShiftRows(self, state):
+        return self._ShiftRows(state, shift_rows_M)
+
+    def ShiftRowsInv(self, state):
+        return self._ShiftRows(state, shift_rows_inverse_M)
+
+    def _ShiftRows(self, state, indices):
+        return [state[index] for index in indices]
+
+    def MixColumns(self, state):
+        return self._MixColumns(state, mix_columns_M)
+
+    def MixColumnsInv(self, state):
+        return self._MixColumns(state, mix_columns_inverse_M)
+
+    def _MixColumns(self, state, matrix):
+        result = []
+        for i in xrange(0, self.block_size/8, 4):
+            """multiply the mix columns matrix with each vector of 4 polynomials;
+            concatenate the result"""
+            result += list(matrix * vector(state[i:i+4]))
+        return result
+
+    @staticmethod
+    def _left_shift(polynomial, shift_by):
+        """Performs a circular left shift on a givaro element"""
+        new_int_value = int(polynomial._int_repr()) << shift_by
+        new_int_value %= (len(gf)-1)
+        return gf._cache.fetch_int(new_int_value)
+
     @staticmethod
     def SubBytes(state):
-        def _left_shift(polynomial, shift_by):
-            """Performs a circular left shift on a givaro element"""
-            new_int_value = int(polynomial._int_repr()) << shift_by
-            new_int_value %= (len(gf)-1)
-            return gf._cache.fetch_int(new_int_value)
         result = []
         for poly in state:
             if poly is not gf(0):
                 inverse = poly ** -1
             else:
                 inverse = gf(0)
-            sbox_result = inverse + _left_shift(inverse, 1) + _left_shift(inverse, 2) + _left_shift(inverse, 3) + _left_shift(inverse, 4) + gf._cache.fetch_int(0x63)
+            sbox_result = inverse + AES._left_shift(inverse, 1) \
+                          + AES._left_shift(inverse, 2) \
+                          + AES._left_shift(inverse, 3) \
+                          + AES._left_shift(inverse, 4) + gf._cache.fetch_int(0x63)
+            result.append(sbox_result)
+        return result
+
+    def SubBytesInv(self, state):
+        result = []
+        for poly in state:
+            # calculate the inverse affine transformation first
+            trans_result = self._left_shift(poly, 1) \
+                   + self._left_shift(poly, 3) \
+                   + self._left_shift(poly, 6) + gf._cache.fetch_int(0x05)
+            if trans_result is not gf(0):
+                sbox_result = trans_result ** -1
+            else:
+                sbox_result = gf(0)
             result.append(sbox_result)
         return result

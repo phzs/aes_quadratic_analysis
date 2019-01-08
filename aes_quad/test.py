a985250d198d79dd8198a2dabc4e56a76bd6cbc1
phzs <zsph1012@hs-karlsruhe.de>
2019-01-08 18:43:10 +0100

    Adds unit test for SubBytesInv

--- aes_quad/test.py
+++ aes_quad/test.py
@@ -11,6 +11,14 @@ class TestAES(unittest.TestCase):
         result = AES.SubBytes([sbox_input])[0]
         self.assertTrue(result == expected_result)
 
+    def test_SubBytesInv(self):
+        sbox_input = gf._cache.fetch_int(0xb8)
+        expected_result = gf._cache.fetch_int(0x9a)
+
+        instance = AES()
+        result = instance.SubBytesInv([sbox_input])[0]
+        self.assertTrue(result == expected_result)
+
     def test_rc(self):
         rc = AESKeySchedule.generate_rc(11) # generates rc for 11 roundkeys
         rc_test = [0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1B, 0x36]

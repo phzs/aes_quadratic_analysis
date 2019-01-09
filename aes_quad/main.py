5c88fec57d62d426c49fb4696654347d1f4189d0
phzs <zsph1012@hs-karlsruhe.de>
2019-01-09 12:23:39 +0100

    Updates main

--- aes_quad/main.py
+++ aes_quad/main.py
@@ -2,10 +2,11 @@
 from sage.all import *
 from AES import AES
 
-instance = AES(key="test", rounds=1, block_size=128)
+instance = AES(key="test", rounds=2, block_size=128)
 
-plaintext = "some message"
+plaintext = "some message to encrypt"
+print "plaintext", len(plaintext)
 ciphertext = instance.encrypt(plaintext)
-#assert instance.decrypt(ciphertext) is plaintext
 
-print instance.get_equations(ciphertext, plaintext, rounds=1)
+print plaintext, "->", " ".join([str(hex(int(c._int_repr())))[2:] for c in ciphertext])
+print "<-", "".join([chr(int(c._int_repr())) for c in instance.decrypt(ciphertext)])

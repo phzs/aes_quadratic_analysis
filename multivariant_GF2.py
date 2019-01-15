dda2a25ead3a8ddb7d30174d14cc413b2b466c5e
phzs <zsph1012@hs-karlsruhe.de>
2019-01-15 10:47:29 +0100

    Removes old and unused scripts

--- multivariant_GF2.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sage
-
-from sage.all import *
-
-var('x y')
-V = VectorSpace(GF(2), 8)
-eq1 = x+y == 9
-eq2 = 2*(x**2)+y == 14
-print(solve([eq1, eq2], x, y))
--- multivariant_R.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sage
-
-from sage.all import *
-
-var('x y z')
-eq1 = x+y == 9
-eq2 = 2*(x**2)+y == 14
-output = latex(solve([eq1, eq2], x, y))
-print output

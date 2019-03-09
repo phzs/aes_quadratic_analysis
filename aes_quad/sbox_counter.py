195beba2e15a4c8edd60e56c5f5f7d0bb54e3bea
phzs <zsph1012@hs-karlsruhe.de>
2019-03-09 20:28:13 +0100

    Updates sbox_counter

--- aes_quad/sbox_counter.py
+++ aes_quad/sbox_counter.py
@@ -1,6 +1,13 @@
 from AES import AES
 
 instance = AES(key="123", debug=True, use_key_schedule=True, rounds=1)
-instance.encrypt("blub")
+instance.encrypt("b"*32)
 
-print "sboxes:", AES.sbox_counter
+print "sboxes 1 round 2 block:", AES.sbox_counter
+
+AES.sbox_counter = 0
+
+instance = AES(key="123", debug=False, use_key_schedule=True, rounds=11)
+instance.encrypt("b"*32)
+
+print "sboxes 1 round 2 block:", AES.sbox_counter
